0a6ca937338228560a9aaa97e82ba5b938e79e65
a25juaosomej-droid <a25juaosomej@inspedralbes.cat>
2026-05-03 01:13:12 +0200

    esto guarda los cambios que se le haga a la incidencia en la base de datos

--- php/administrador.php
+++ php/administrador.php
@@ -1,1 +1,20 @@
+<?php
+date_default_timezone_set('Europe/Madrid');
+require_once 'conexion.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id    = intval($_POST['id_incidencia']);
+    $tec   = !empty($_POST['tecnico'])     ? intval($_POST['tecnico'])     : "NULL";
+    $tip   = !empty($_POST['tipo'])        ? intval($_POST['tipo'])        : "NULL";
+    $dep   = !empty($_POST['departamento'])? intval($_POST['departamento']): "NULL";
+    $prio  = in_array($_POST['prioritat'], ['Alta','Media','Baja']) ? "'".$_POST['prioritat']."'" : "NULL";
+
+
+    $conn->query("UPDATE incidencia SET tecnico=$tec, tipo=$tip, departamento=$dep, prioritat=$prio WHERE id_incidencia=$id");
+    header("Location: administrador.php");
+   
+   exit;
+}
+
 
