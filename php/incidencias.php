6d7e6c8f796741d9e942c7fab7077410b968996a
a25juaosomej-droid <a25juaosomej@inspedralbes.cat>
2026-05-06 08:18:26 +0200

    Esta parte recoge los datos que entren y comprueba que no estén vacíos,despues los guarda en la BD

--- php/incidencias.php
+++ php/incidencias.php
@@ -4,3 +4,24 @@ require_once 'conexion.php';
 
 $error    = "";
 $missatge = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $titol        = trim($conn->real_escape_string($_POST['titol']));
+    $descripcion  = trim($conn->real_escape_string($_POST['descripcion']));
+    $departamento = intval($_POST['departamento']);
+
+    if (empty($titol) || empty($descripcion) || $departamento === 0) {
+        $error = "Tots els camps són obligatoris.";
+    } else {
+        $sql = "INSERT INTO incidencia (titol, descripcion, data, departamento)
+                VALUES ('$titol', '$descripcion', NOW(), $departamento)";
+
+
+        if ($conn->query($sql)) {
+            $missatge = "Incidència registrada. ID: " . $conn->insert_id;
+        } else {
+            $error = "Error: " . $conn->error;
+        }
+    }
+}
